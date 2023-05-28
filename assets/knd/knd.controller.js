d2ce91c7dfab15e229fab56da6113fad44eaca22
topranksmm <76875181+topranksmm@users.noreply.github.com>
2023-05-29 05:18:58 +0800

    Add files via upload

---
+++ assets/knd/knd.controller.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * topranksmm Theme v1.0
+ * 
+ * created by suleyman kandilci
+ * designed by can aydin
+ * 
+ * contact us: suleymankndlc@outlook.com
+ *             duzst97@gmail.com
+*/
+
+import './js/selectbox.js';
+
